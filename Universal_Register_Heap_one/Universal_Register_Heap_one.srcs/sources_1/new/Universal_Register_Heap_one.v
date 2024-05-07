ad992d95db92ba93976ad981aeaa1b2b78b5b53d
RaphaelNY <1278173058@qq.com>
2024-05-07 11:37:24 +0800

    init

---
+++ Universal_Register_Heap_one/Universal_Register_Heap_one.srcs/sources_1/new/Universal_Register_Heap_one.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/05/07 11:37:04
+// Design Name: 
+// Module Name: Universal_Register_Heap_one
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Universal_Register_Heap_one(
+
+    );
+endmodule
